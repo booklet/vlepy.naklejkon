90aafd31c3efa3fddcff238e251d8e12f32353dc
patrykb15580 <patrykb15580@gmail.com>
2019-07-29 12:36:14 +0200

    chore: update title and meta description tags

--- app/views/layout/app.php
+++ app/views/layout/app.php
@@ -1,12 +1,13 @@
 <!DOCTYPE html>
 <html lang="pl" class="no-js">
     <head>
-        <meta charset="utf-8">
-        <meta http-equiv="X-UA-Compatible" content="IE=edge">
-        <meta content="noodp" name="robots">
-        <meta content="noydir" name="robots">
-        <title>Vlepy.pl</title>
-        <meta content="width=device-width, initial-scale=1.0" name="viewport">
+        <meta charset="utf-8" />
+        <meta http-equiv="X-UA-Compatible" content="IE=edge" />
+        <meta content="noodp" name="robots" />
+        <meta content="noydir" name="robots" />
+        <title>Vlepy.pl - druk vlepek w 24h</title>
+        <meta name="description" content="Producent vlepek. Druk i wysyłka w 24h. Szybkie i wygodne zamawianie vlepek. Drukujemy wlpeki w różnych wymiarach." />
+        <meta content="width=device-width, initial-scale=1.0" name="viewport" />
         <?php include 'app/views/shared/_google_tag_manager.php' ?>
         <?php include 'app/views/shared/_google_analytics.php' ?>
 
