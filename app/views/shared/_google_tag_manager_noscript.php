e99132958981f99021aa27725ac2d2c1e6b73a2b
Bartosz Mikolajczak <b.mikolajczak@booklet.pl>
2019-07-31 18:17:07 +0200

    chore: change google tag manager account id

--- app/views/shared/_google_tag_manager_noscript.php
+++ app/views/shared/_google_tag_manager_noscript.php
@@ -1,6 +1,6 @@
 <?php if (Config::get('env') == 'production'): ?>
   <!-- Google Tag Manager (noscript) -->
-  <noscript><iframe src="https://www.googletagmanager.com/ns.html?id=GTM-KZD3TW2"
+  <noscript><iframe src="https://www.googletagmanager.com/ns.html?id=GTM-TQGD3J2"
   height="0" width="0" style="display:none;visibility:hidden"></iframe></noscript>
   <!-- End Google Tag Manager (noscript) -->
 <?php endif; ?>
