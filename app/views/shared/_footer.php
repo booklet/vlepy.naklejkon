f16f17328edd6853a0c0a9c726504d1b7d50a5f3
patrykb15580 <patrykb15580@gmail.com>
2019-08-09 14:17:02 +0200

    feat: update footer

--- app/views/shared/_footer.php
+++ app/views/shared/_footer.php
@@ -3,19 +3,16 @@
         <div class="container container--1200">
             <div class="flexbox flexbox--grid">
                 <div class="footer__logo col col--18p">
-                    <img src="/public/assets/images/naklejkon_logo_white.svg" alt="" />
+                    <img src="/public/assets/images/logos/Vlepy_PL_logo.svg" alt="" />
                 </div>
-                <div class="footer__contact col col--38p">
-                    <a class="anchor" name="kontakt"></a>
-                    <b>+48 62 307 07 12</b> lub <b>info@naklejkon.pl</b><br />
-                    od poniedziałku do piątku w godzinach 7:00 - 16:00
-                    <p>
-                        Za połaczenia z polskich telefonów stacjonarnych<br />
-                        i komórkowych naliczana jest opłata taryfikacyjna<br />
-                        według stawek operatora osoby dzwoniącej.
-                    </p>
+                <div class="col col--60p">
+                    <div class="footer__contact">
+                        <a class="anchor" name="kontakt"></a>
+                        <b>info@vlepy.pl</b><br />
+                        od poniedziałku do piątku w godzinach 7:00 - 16:00
+                    </div>
                 </div>
-                <div class="col col--44p">
+                <div class="col col--22p">
                     <div class="footer__social">
                         <a class="social-link social-link--facebook fab fa-facebook-square" href="http://facebook.com/vlepypl" target="_blank"></a>
                         <a class="social-link social-link--instagram fab fa-instagram" href="http://instagram.com/vlepy" target="_blank"></a>
@@ -24,13 +21,13 @@
                         <li><a href="http://naklejkon.pl/article/regulamin">Regulamin</a></li>
                         <li><a href="http://naklejkon.pl/article/polityka-prywatnosci">Polityka prywatności</a></li>
                     </ul>
-                    <div class="footer__copyright">
-                        Właścicielem serwisu naklejkon.pl jest Booklet Group S.C.
-                        ul. Korzkwy 23, 63-300 Pleszew<br />
-                        © Copyright <?= date('Y') ?> by Booklet Group S.C.
-                        Wszystkie prawa zastrzeżone / All rights reserved.
-                    </div>
                 </div>
+            </div>
+            <div class="footer__copyright">
+                Właścicielem serwisu naklejkon.pl jest Booklet Group S.C.
+                ul. Korzkwy 23, 63-300 Pleszew<br />
+                © Copyright <?= date('Y') ?> by Booklet Group S.C.
+                Wszystkie prawa zastrzeżone / All rights reserved.
             </div>
         </div>
     </div>
